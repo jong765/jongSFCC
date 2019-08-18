f4accb9756e806fe829fd45b9154db9c5f6489b9
jkim <jkim@pacsun.com>
2019-08-18 14:13:47 -0700

    2

--- JongTest/cartridge/scripts/LoyaltyPlus/service/RewardsService.js
+++ JongTest/cartridge/scripts/LoyaltyPlus/service/RewardsService.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const ServiceRegistry = require('dw/svc/ServiceRegistry');
-const Status = require('dw/system/Status');
-const Site = require('dw/system/Site');
-const RewardsRequest = require('../serviceRequest/RewardsRequest');
-const Util = require('../util/Util');
-const UrlPath = require('../util/LoyaltyPlusConstants').UrlPath;
+var ServiceRegistry = require('dw/svc/ServiceRegistry');
+var Status = require('dw/system/Status');
+var Site = require('dw/system/Site');
+var RewardsRequest = require('../serviceRequest/RewardsRequest');
+var Util = require('../util/Util');
+var UrlPath = require('../util/LoyaltyPlusConstants').UrlPath;
 
 exports.rewards = function () {
-    let data = {
+    var data = {
         urlPath       : UrlPath.REWARDS,
         requestMethod : 'GET',
         request       : RewardsRequest.getRewardsRequest()
     };
 
-    let result = Util.callService(data);
+    var result = Util.callService(data);
     return result;
 };
--- JongTest/cartridge/scripts/LoyaltyPlus/util/Util.js
+++ JongTest/cartridge/scripts/LoyaltyPlus/util/Util.js
@@ -1,25 +1,25 @@
 'use strict';
 
-const MessageDigest = require('dw/crypto/MessageDigest');
-const Encoding = require('dw/crypto/Encoding');
-const Bytes = require('dw/util/Bytes');
-const CustomPreference = require('../util/LoyaltyPlusConstants').CustomPreference;
+var MessageDigest = require('dw/crypto/MessageDigest');
+var Encoding = require('dw/crypto/Encoding');
+var Bytes = require('dw/util/Bytes');
+var CustomPreference = require('../util/LoyaltyPlusConstants').CustomPreference;
 
 var Util = {};
 
 Util.callService = function (data) {
-	let loyaltyPlusServiceInit = require('~/cartridge/scripts/LoyaltyPlus/init/LoyaltyPlusServiceInit');
-    let service = loyaltyPlusServiceInit.LoyaltyPlusService('loyaltyplus.http.default');
+	var loyaltyPlusServiceInit = require('~/cartridge/scripts/LoyaltyPlus/init/LoyaltyPlusServiceInit');
+    var service = loyaltyPlusServiceInit.LoyaltyPlusService('loyaltyplus.http.default');
     service.addParam("uuid", data.uuid);
     service.addParam("sig", data.sig);
-    let response = service.call(data);
+    var response = service.call(data);
     return response;
 }
 
 Util.getSignature = function (params) {
-    let secretKey = CustomPreference.SECRET_KEY;
+    var secretKey = CustomPreference.SECRET_KEY;
 
-    let keys = [];
+    var keys = [];
     for (var key in params) {
         if (params.hasOwnProperty(key)) { 
             keys.push(key); 
@@ -27,14 +27,14 @@ Util.getSignature = function (params) {
     };
     keys.sort();
 
-    let dataToSign = secretKey;
+    var dataToSign = secretKey;
 
     for (var i=0; i<keys.length; i++){
         dataToSign += keys[i]+params[keys[i]];
     }
 
-    let encryptor : MessageDigest = new MessageDigest('MD5');
-    let signature = Encoding.toHex(encryptor.digestBytes(new Bytes(dataToSign)));
+    var encryptor : MessageDigest = new MessageDigest('MD5');
+    var signature = Encoding.toHex(encryptor.digestBytes(new Bytes(dataToSign)));
     return signature;
 }
 
