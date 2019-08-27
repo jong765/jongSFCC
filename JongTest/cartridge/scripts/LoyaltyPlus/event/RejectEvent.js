77a1f6d1b5750b26040418d6bd3f2701454a3ef8
jong7 <jong7@LAPTOP-B1QGNGL4>
2019-08-26 21:58:56 -0700

    1

---
+++ JongTest/cartridge/scripts/LoyaltyPlus/event/RejectEvent.js
@@ -0,0 +1,41 @@
+/**
+ * ShowCustomer.js
+ * 
+ *  Get customer information
+ *
+ *   @input emailAddress : String
+ *   @input extCustomerId : String
+ *   @input vendor : String
+ *   @input vendorId : String
+ *   @input include : String
+ *   @output serviceResponse : Object
+ * 
+ */
+
+var CustomerShowService = require('../service/CustomerShowService');
+var Logger = require('dw/system/Logger');
+var logger = Logger.getLogger("loyaltyplus-error", "ShowCustomer.js");
+
+function execute(args) {
+	var response = run(args.emailAddress, args.extCustomerId, args.vendor, args.vendorId);
+    args.response = response;
+    return response.success ? PIPELET_NEXT : PIPELET_ERROR;
+}
+
+function run(emailAddress, extCustomerId, vendor, vendorId, include) {
+    var response = null;
+
+    try {
+    	response = CustomerShowService.run(emailAddress, extCustomerId, vendor, vendorId, include);
+    } catch (e) {
+        var exception = e;
+        var errMessage = exception.message + "\n" + exception.stack;
+        logger.error(errMessage);
+    }
+    return response;
+}
+
+module.exports = {
+    'execute': execute,
+    'run': run
+}
